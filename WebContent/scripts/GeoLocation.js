4ba868b178c6392a34bd2160a2a3c1d290acb766
sand89 <ramamoorthy.s@husky.neu.edu>
2015-11-06 15:12:33 -0500

    Added Home.jsp and GeoLocation.js

---
+++ WebContent/scripts/GeoLocation.js
@@ -0,0 +1,27 @@
+/**
+ * Author: Sandeep Ramamoorthy
+ * Date: 11/6/2015 12:57 AM
+ * Description: Contains the code snippet to detect the geographical
+ * location of the user. This code snippet is taken from Mozilla Developers Network
+ * (https://developer.mozilla.org/en-US/docs/Web/API/Geolocation/getCurrentPosition)
+ */
+var options = {
+  enableHighAccuracy: true,
+  timeout: 5000,
+  maximumAge: 0
+};
+
+function success(pos) {
+  var crd = pos.coords;
+
+  console.log('Your current position is:');
+  console.log('Latitude : ' + crd.latitude);
+  console.log('Longitude: ' + crd.longitude);
+};
+
+function error(err) {
+  console.warn('ERROR(' + err.code + '): ' + err.message);
+};
+function getLocation() {
+    navigator.geolocation.getCurrentPosition(success, error, options);
+}
