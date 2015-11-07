2759f5b50109fb661acef5b032f342f2846928f8
karthik <karthik@ccs.neu.edu>
2015-11-06 21:08:08 -0500

    Better Strcuture
    JERKS

---
+++ WHAMv0.1/src/edu/neu/cs5500/Jerks/business/EventManager.java
@@ -0,0 +1,10 @@
+package edu.neu.cs5500.Jerks.business;
+
+/* Author: Sandeep Ramamoorthy
+ * Creation Date: 11/02/2015 6:00 AM EST
+ * Description: All business logic related to events goes into this class 
+ * */
+public class EventManager {
+
+	//Remove placeholder 
+}
--- src/dbProviders/EventProvider.java
+++
@@ -1,9 +0,0 @@
-package dbProviders;
-
-/* Author: Sandeep Ramamoorthy
- * Creation Date: 11/02/2015 6:04 AM EST
- * Description: All event related database calls and methods goes into this class 
- * */
-public class EventProvider {
-
-}
